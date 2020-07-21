825334ece967ed42b5c811a8ca0a6d9bb436e9d8
Marouane Moumni <57830533+M-Marou@users.noreply.github.com>
2020-07-21 17:00:20 +0100

    Added SelectOne function
    this function returns only one record from database table!

--- app/database/db.php
+++ app/database/db.php
@@ -8,6 +8,16 @@ function dd($value){
     die();
 }
 
+function executeQuery($sql, $data){
+    global $conn;
+    $stmnt = $conn->prepare($sql);
+    $values = array_values($data);
+    $types = str_repeat('s', count($values));
+    $stmnt->bind_param($types, ...$values);
+    $stmnt->execute();
+    return $stmnt;
+}
+
 // function to return all records in database table: 
 function selectALL($table , $conditions = []){
 
@@ -28,14 +38,31 @@ function selectALL($table , $conditions = []){
             }
             $i++;
         }
-        $stmnt = $conn->prepare($sql);
-        $values = array_values($conditions);
-        $types = str_repeat('s', count($values));
-        $stmnt->bind_param($types, ...$values);
-        $stmnt->execute();
+        $stmnt = executeQuery($sql, $conditions);
         $records = $stmnt->get_result()->fetch_all(MYSQLI_ASSOC);
         return $records;
     }
+}
+
+function selectOne($table , $conditions){
+
+    global $conn;
+    $sql = "SELECT * FROM $table";
+
+        $i = 0;
+        foreach ($conditions as $key => $value){
+            if($i===0){
+                $sql = $sql . " WHERE $key=?";
+            } else {
+                $sql = $sql . " AND $key=?";
+            }
+            $i++;
+        }
+
+        $sql = $sql . " LIMIT 1";
+        $stmnt = executeQuery($sql, $conditions);
+        $records = $stmnt->get_result()->fetch_assoc();
+        return $records;
 }
 
 $conditions = [
@@ -43,6 +70,6 @@ $conditions = [
     'username' => 'marou'
 ];
 
-$users = selectALL('users', $conditions);
+$users = selectOne('users', $conditions);
 
 dd($users);
