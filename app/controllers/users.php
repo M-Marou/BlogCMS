acca98687c35eee2db7366f2a5e3e961b0334181
Marouane Moumni <57830533+M-Marou@users.noreply.github.com>
2020-07-23 16:50:52 +0100

    Added form validation
    added form validation to dispaly errors such as "username is required " also check if password match or not, also the form now keeps the data dispalyed even when there is errors !

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -1,11 +1,30 @@
 <?php 
     include(ROOT_PATH . "/app/database/db.php");
+    include(ROOT_PATH . "/app/helpers/validateUser.php");
+
+    $username = '';
+    $email = '';
+    $password = '';
+    $passwordConf = '';
+
+    $errors = array();
+
     if (isset($_POST['register-btn'])){
-        unset($_POST['register-btn'], $_POST['passwordConf']);
-        $_POST['admin'] = 0;
-        $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $user_id = create('users', $_POST);
-        $user = selectOne('users', ['id' => $user_id]);
-        dd($_POST);
+
+         $errors = validateUser($_POST);
+         
+        if(count($errors) === 0){
+            unset($_POST['register-btn'], $_POST['passwordConf']);
+            $_POST['admin'] = 0;
+            $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $user_id = create('users', $_POST);
+            $user = selectOne('users', ['id' => $user_id]);
+
+             dd($user);
+        } else {
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $passwordConf = $_POST['passwordConf'];
+        }
     }
-?>
